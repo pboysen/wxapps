a7e0aeeeef43a0ce2b9c8db92ba1579aa4de585a
Pete Boysen <pboysen@pboysen-PC>
2019-11-03 20:44:00 -0600

    added additional dialogs

--- src/pdfsim/pageviewer.js
+++ src/pdfsim/pageviewer.js
@@ -6,12 +6,14 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed toF in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import {getStore} from "../utils"
+let store = getStore(),searchParams = new URLSearchParams(window.location.search.substring(1))
 
 'use strict';
 
