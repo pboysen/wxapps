31b0a0d5c4d59a24fc2c3b5e4020a9ce1dacefa3
Pete Boysen <pboysen@pboysen-PC>
2021-02-16 13:14:42 -0600

    updated mammoth code

---
+++ node_modules/mammoth/node_modules/browserify-prepend-licenses/README.md
@@ -0,0 +1,13 @@
+# browserify-license-sniffer
+
+A browserify plugin to prepend bundles with the licenses of included packages.
+
+## Installation
+
+    npm install browserify-license-sniffer
+
+## Usage
+
+Use as a browserify plugin. For instance, with the CLI:
+
+    browserify -p browserify-license-sniffer
--- src/mammoth/mammoth.js
+++ src/mammoth/mammoth.js
@@ -1,82 +1,69 @@
 import md5 from "md5";
- 
-function getWidget(imageBuffer) {
-	var hash = md5(imageBuffer);
-	return {
-		type: "textfield",
-		length: 10,
-		etc: ""
-	}
-}
- 
-var options = {
-    convertImage: mammoth.images.imgElement(function(image) {
-    	image.altText = "";
-        return image.read("base64").then(function(imageBuffer) {
-        	var props = getWidget(imageBuffer);
-        	if (props) return {
-            	"class": "widget",
-            	props: JSON.stringify(props)
-        	}
-        	else return {
-        		alt: "whatever",
-        		src: "data:" + image.contentType + ";base64," + imageBuffer
-            };
-        });
-    }),
-    styleMap: [
-    	"p[style-name='MultipleChoice'] => ul > li.mchoice:fresh", 
-    	"p[style-name='Checklist'] => ul > li.chklist:fresh",
-    	"br[type='page'] => div.page:fresh"
-    ]
-};
+import mammoth from "C:/Users/pboysen/git/mammoth.js/lib/index.js";
 
 const inputElement = document.getElementById("input");
-inputElement.addEventListener("change", handleFiles, false);
+inputElement.addEventListener("change", readFile, false);
 
-function handleList(listType, inputType, prefix) {
-	let nameIdx = 1;
-	let valueIdx = 1;
-	let nodes = document.getElementsByClassName(listType);
-	for (var i = 0, len = nodes.length; i < len; i++) {
-		let li = nodes[i];
-		var input = document.createElement("input");
-		input.setAttribute("type", inputType);
-		input.setAttribute("name", prefix + nameIdx);
-		input.setAttribute("value", valueIdx++);
-		li.prepend(input);
-		if (listType == "chklist") nameIdx++;
-		if (!li.nextSibling) valueIdx = 1;
-	};
-}
-
-function handleWidgets() {
-	var nodes = document.getElementsByClassName("widget");
-	for (var i = 0, len = nodes.length; i < len; i++) {
-		var img = nodes[i];
-		var div = document.createElement("div");
-		div.setAttribute("props", img.getAttribute("props"));
-		div.setAttribute("class", "widget");
-		img.parentNode.replaceChild(div, img);
-	}
-}
-
-function handleTD(src) {
-	return src.replace(/<td><p>/g, "<td>").replace(/<\/p><\/td>/g, "</td>");
-}
-
-function handleFiles() {
+function readFile() {
   var reader = new FileReader();
-  reader.onload = function(e) { 
+  reader.onload = function(e) {
 		mammoth.convertToHtml({arrayBuffer: reader.result}, options)
 	    .then(function(result) {
-	        document.getElementById("content").innerHTML = handleTD(result.value);
-	        handleList("mchoice", "radio", "mc");
-	        handleList("chklist", "checkbox", "cl");
-	        handleWidgets()
-	        console.log(document.getElementById("content").innerHTML);
+	    	let value = transform(result.value);
+	        document.getElementById("content").innerHTML = value
+	        console.log(value.split("<p><page /></p>"));
 	    })
 	    .done();
   };
   reader.readAsArrayBuffer(this.files[0]);
+}
+
+var widgetProps = {
+		fb122b7ad0194a68d3708ea5a1ace9af: { type: "textfield", length: 10,  etc: "" }
+}
+
+var options = {
+    styleMap: [
+    	"p[style-name='MultipleChoice'] => ul.mchoice > li:fresh", 
+    	"p[style-name='Checklist'] => ul.chklist > li:fresh"
+    ]
+};
+
+function inputHTML(inputType, name, value) {
+	return ["<input type=\"", inputType, "\" name=\"",name,"\" value=\"", value, "\">"].join("");
+}
+
+function liInputs(listItems, inputType, name) {
+	var liRE = /<li>(.*?)<\/li>/g;
+	let value = 1;
+	return listItems.replaceAll(liRE, (match, liText) => {
+		let choiceName = inputType == "checkbox"? name + "." + value : name;
+		return ["<li>", inputHTML(inputType, choiceName, value++), liText, "</li>"].join("");
+	});
+}
+
+function transformList(html, type, inputType, prefix) {
+	var ulRE = new RegExp("<ul class=\"" + type + "\">(.*?)<\/ul>","g");
+	let quest = 1;
+	return html.replaceAll(ulRE, (match, listItems) => {
+		return ["<ul class=\"", type, "\">", liInputs(listItems, inputType, prefix + quest++), "</ul>", ].join("");
+	});
+}
+
+var replacements = [
+	{ from: /<td([^>]*)><p[^>]*>([^<]*)<\/p>(<p>[^<]*<\/p>)*<\/td>/g,
+	  to: "<td$1>$2$3</td>" },
+	{ from: /<img src="data:([^;]*);base64,([^"]*)"[^\/]*\/>/g,
+	  to: (match, type, data) => {
+		var props = widgetProps[md5(data)];
+		return props ? "<div class=\"widget\" props=\""+JSON.stringify(props)+"\"></div>" : match; 
+	  }
+	}
+]
+
+function transform(html) {
+	let result = replacements.reduce((result, repl) => result.replace(repl.from, repl.to), html);
+	result = transformList(result, "mchoice", "radio", "mc");	
+	result = transformList(result, "chklist", "checkbox", "cl");
+	return result;
 }
